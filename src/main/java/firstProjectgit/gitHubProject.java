695d7fbeaf85fe697e5eded8fb8bf67f389167cb
Daiyana <baladaiyana@gmail.com>
2025-09-15 05:11:24 +0530

    selenium some codes add in gitfirstdemo

--- src/main/java/firstProjectgit/gitHubProject.java
+++ src/main/java/firstProjectgit/gitHubProject.java
@@ -5,6 +5,8 @@ public class gitHubProject {
 	public static void main(String[] args) {
 		System.out.println(" Hello World! ");
 	}
-
+	public void firstgit() {
+		System.out.println("first demo github");
+	}
 }
 
